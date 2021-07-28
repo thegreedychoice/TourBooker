fd36e7df4101f83bb6edbfe6b5551592f8f1f1de
Shubham Shukla <shubham.shukla@citrix.com>
2021-07-28 17:26:50 -0400

    first commit

---
+++ TourBooker.Logic/AppData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.AdvCShColls.TourBooker.Logic
+{
+	public class AppData
+	{
+		public List<Country> AllCountries { get; private set; }
+		public void Initialize(string csvFilePath)
+		{
+			CsvReader reader = new CsvReader(csvFilePath);
+			this.AllCountries = reader.ReadAllCountries().OrderBy(x => x.Name).ToList();
+
+			// during the module, the above line is changed to
+			//this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
+
+
+		}
+	}
+}
